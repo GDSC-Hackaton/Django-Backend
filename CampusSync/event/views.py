fb0f031b1c7c74fd591c8e36ac7a5ecab0a71297
Nathnael Yirga <natty@localhost.localdomain>
2024-04-02 19:49:25 +0300

    custom user auth, manager ...

--- CampusSync/event/views.py
+++ CampusSync/event/views.py
@@ -1,15 +1,7 @@
-from django.shortcuts import get_object_or_404, render, HttpResponse, redirect
-from django.http import Http404, JsonResponse
-from user.models import Host
 from .serializer import EventSerializer
 from .models import Event
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework import permissions
-from rest_framework.response import Response
-from rest_framework import generics
-from drf_yasg.utils import swagger_auto_schema
+# from drf_yasg.utils import swagger_auto_schema
 from rest_framework import viewsets
-from django.views.decorators.csrf import csrf_exempt
  
 class EventViewSet(viewsets.ModelViewSet):
     """
