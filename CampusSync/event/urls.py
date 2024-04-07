aaf12e446b5b6870f9b95ecb1395172788a8f62b
Mihretthe <mihretekalgn@gmail.com>
2024-04-07 20:55:29 +0300

    Adding Search and Filter Logic

--- CampusSync/event/urls.py
+++ CampusSync/event/urls.py
@@ -1,4 +1,4 @@
-from .views import EventViewSet, event_notifications
+from .views import EventViewSet, event_notifications, search, order_by_old, order_by_downvote,order_by_recent,order_by_upvote
 from rest_framework.routers import DefaultRouter
 from django.urls import path
 
@@ -7,6 +7,11 @@ router = DefaultRouter()
 router.register(r'events', EventViewSet, basename='event')
 urlpatterns = router.urls + [
     path('event-notifications/', event_notifications),
+    path('event-search/', search),
+    path('event-order-recent/', order_by_recent),
+    path('event-order-upvote/', order_by_upvote),
+    path('event-order-downvote/', order_by_downvote),
+    path('event-order-old/', order_by_old),
 ]
 
  
--- CampusSync/event/views.py
+++ CampusSync/event/views.py
@@ -7,6 +7,8 @@ from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from drf_spectacular.utils import extend_schema, OpenApiExample, inline_serializer
 
+from rest_framework import status
+
 
 class EventViewSet(viewsets.ModelViewSet):
     """
@@ -38,6 +40,74 @@ def event_notifications(request):
     return Response({'status': 'Failed, no such event'})
 
 
+"""
+<button onClick={() => handleFilter("recent")}>recent</button>
+<button onClick={() => handleFilter("old")}>old</button>
+<button onClick={() => handleFilter("upvote")}>upvote</button>
+<button onClick={() => handleFilter("downvote")}>downvote</button>
+"""
+    
+
+@api_view(['GET'])
+def order_by_recent(request):
+    if request.method != 'GET':
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    
+    events = Event.objects.order_by("-recent")
+    return Response(events.values())
+
+@api_view(['GET'])
+def order_by_old(request):
+    if request.method != 'GET':
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    
+    events = Event.objects.order_by("recent")
+    return Response(events.values())
+
+
+@api_view(['GET'])
+def order_by_upvote(request):
+    if request.method != 'GET':
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    
+    events = Event.objects.order_by("-upvote")
+    return Response(events.values())
+
+@api_view(['GET'])
+def order_by_downvote(request):
+    if request.method != 'GET':
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    
+    events = Event.objects.order_by("-downvote")
+    return Response(events.values())
+
+
+
+
 def custom_404(request, exception):
     print("$$")
     return render(request, 'event/404.html', status=404)
+
+
+
+@api_view(['GET'])
+def search(request):
+    if request.method != 'GET':
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    
+    if 'event_name' not in request.data:
+        return Response({'error': 'Missing required field: event_name'}, status=status.HTTP_400_BAD_REQUEST)
+
+    event_name = request.data['event_name']
+    events = Event.objects.filter(name__icontains=event_name)
+
+    if not events.exists():
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    # Return the list of events
+    return Response(events.values()) 
+
+
+
+
