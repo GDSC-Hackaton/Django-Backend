ea7a9927db7403a30949f49125868932de1dd343
Fasikagebrehana <fasikagebrehana1@gmail.com>
2024-04-09 14:50:20 +0300

    Upvote and Downvote for events

--- CampusSync/event/urls.py
+++ CampusSync/event/urls.py
@@ -7,6 +7,8 @@ router = DefaultRouter()
 router.register(r'events', EventViewSet, basename='event')
 urlpatterns = router.urls + [
     path('event-notifications/', event_notifications),
+    path('events/<int:event_id>/upvote/', views.upvote_event, name='upvote_event'),
+    path('events/<int:event_id>/downvote/', views.downvote_event, name='downvote_event')
 ]
 
  
